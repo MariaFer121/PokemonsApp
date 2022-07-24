051a855ffe339578abf552647809ba347e245342
Maria fernanda <95837379+MariaFer121@users.noreply.github.com>
2022-07-24 14:11:46 -0400

    Creacion de imgen en js para luego ser importada

---
+++ src/img/img.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Letra from "../img/Letra.png"
+//funcion para poder subir una imagen
+const Imagen = (props) => {
+    return (
+        <img src={Letra} alt="imagen de prueba"/>
+
+    )
+}
+
+export default Imagen
