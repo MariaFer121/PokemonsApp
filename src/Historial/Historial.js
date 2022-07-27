518ecb1ec21bb4e9ebfd49d848e3f4cf84014995
Maria fernanda <95837379+MariaFer121@users.noreply.github.com>
2022-07-26 23:42:42 -0400

    Creacion de la logica

--- src/Historial/Historial.js
+++ src/Historial/Historial.js
@@ -1,9 +1,43 @@
 import React from "react"
+import {
+    Input,
+    Button
+} from "@mui/material"
 
 const Historial = (props) => {
+    const [busqueda, setBusqueda] = useState('')
+    const [pokemon, setPokemon] = useState('')
+    // funcion busqueda de pokemon 
+    const busquedaPokemon = async (pokemon) => {
+        try {
+            let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        } catch (err) { }
+    }
+
+
+    const onChange = (e) => {
+        setBusqueda(e.target.value)
+    }
+    const onClick = async (e) => {
+        const data = await busquedaPokemon(busqueda)
+        setPokemon(data)
+    }
+
     return (
         <>
-            Historial de Pokemones !
+            <Input
+                placeholder="Buscar Pokemon"
+                type='text'
+                onChange={onChange}
+                value={busqueda}>
+            </Input>
+            <Button
+                type='submit'
+                onClick={(e) => onClick(e)}>Buscar</Button>
+
 
         </>
     )
